4099dbc688e8cbb18fd237026038fe8c34a84331
Ekaterina <43721355+katunya18@users.noreply.github.com>
2019-12-06 20:15:36 +0300

    Update Main.java

--- laba3/Main.java
+++ laba3/Main.java
@@ -1,10 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
 public class Main {
+
     public static void main(String[] args) {
-    Book telephonebook1 = new Spravochnik("123-9781.256",
-                      "Боич А.А", "Как прекрасно жить",2016,"Москва");
-    Book telephonebook2 = new Enciklopediya("659-7814.456",
-                "Душко М.В", "Всё о диких животных",2005,"Набережные Челны");
-        System.out.println(telephonebook1);
-        System.out.println(telephonebook2);
+
+        Book telephonebook1 = new Spravochnik("123-9781.256",
+
+                "Боич А.А", " ", 2016, "Москва", "телефонов");
+
+        Book telephonebook2 = new Enciklopediya("659-7814.456",
+
+                "Душко М.В", "Всё о диких животных", 2005, "Набережные Челны", 123);
+
+        List<Book> lst = Arrays.asList(telephonebook1, telephonebook2);
+
+        for (Book book : lst) {
+            book.Information();
+        }
+        for (Book book : lst) {
+            book.Show();
+        }
     }
 }
