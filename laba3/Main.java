b718663239d708bbec79d74a032d9df0920233a4
Ekaterina <43721355+katunya18@users.noreply.github.com>
2019-12-08 11:30:08 +0300

    Update Main.java

--- laba3/Main.java
+++ laba3/Main.java
@@ -4,21 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book telephonebook1 = new Spravochnik("123-9781.256",
+        Spravochnik telephonebook1 = new Spravochnik("123-9781.256",
 
                 "Боич А.А", " ", 2016, "Москва", "телефонов");
 
-        Book telephonebook2 = new Enciklopediya("659-7814.456",
+        Enciklopediya telephonebook2 = new Enciklopediya("659-7814.456",
 
                 "Душко М.В", "Всё о диких животных", 2005, "Набережные Челны", 123);
 
-        List<Book> lst = Arrays.asList(telephonebook1, telephonebook2);
-
-        for (Book book : lst) {
-            book.Information();
-        }
-        for (Book book : lst) {
-            book.Show();
-        }
+        List<Book> lst = Arrays.asList(telephonebook1, telephonebook2)
     }
 }
